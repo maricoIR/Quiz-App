068c5393678ee8390cf569b9741fe0ef1d688774
maricoIR <matin.kahrizi@gmail.com>
2022-07-12 14:20:25 +0430

    Questions Array

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    id: 0,
+    title: "What nut is used to make marzipan?",
+    options: [
+      { title: "Pistachio", id: 1, questionId: 0 },
+      { title: "b", id: 2, questionId: 0 },
+      { title: "Dorost", id: 3, questionId: 0 },
+      { title: "d", id: 4, questionId: 0 },
+    ],
+    correctOption: { id: 3 },
+  },
+  {
+    id: 1,
+    title: "What is your favorite food ?",
+    options: [
+      { title: "aa", id: 1, questionId: 1 },
+      { title: "Dorost", id: 2, questionId: 1 },
+      { title: "cc", id: 3, questionId: 1 },
+      { title: "dd", id: 4, questionId: 1 },
+    ],
+    correctOption: { id: 2 },
+  },
+  {
+    id: 2,
+    title: "what is 2+2 ?",
+    options: [
+      { title: "Dorost", id: 1, questionId: 2 },
+      { title: "0", id: 2, questionId: 2 },
+      { title: "3", id: 3, questionId: 2 },
+      { title: "6", id: 4, questionId: 2 },
+    ],
+    correctOption: { id: 1 },
+  },
+  {
+    id: 3,
+    title: "what is BBC stands for ?",
+    options: [
+      { title: "idk", id: 1, questionId: 3 },
+      { title: "0", id: 2, questionId: 3 },
+      { title: "3", id: 3, questionId: 3 },
+      { title: "Dorost", id: 4, questionId: 3 },
+    ],
+    correctOption: { id: 4 },
+  },
+];
